d462c973c0c9a2cdde15d0197593a0b0634c4f2a
Michael Brickler <mbrick02@gmail.com>
2013-08-08 10:23:09 -0400

    addDish form v0

---
+++ addDish.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+       Remove this if you use the .htaccess -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+  <title>addDish</title>
+  <meta name="description" content="" />
+  <meta name="author" content="bricklerml" />
+
+  <meta name="viewport" content="width=device-width; initial-scale=1.0" />
+
+    <link rel="stylesheet" href="./includes/comon.css" type="text/css" />
+  <script type="text/javascript" src="./includes/modernizr-1.7.js"></script>
+  <script type="text/javascript" src="./includes/jquery-1.5.1.js"></script>
+  <script type="text/javascript" src="./includes/placholder.js"></script>
+
+  <!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+  <link rel="shortcut icon" href="/favicon.ico" />
+</head>
+
+<body>
+  <div>
+    <header>
+      <h1>addDish</h1>
+    </header>
+    <nav>
+      <p><a href="/">Home</a></p>
+      
+    </nav>
+
+    <div>
+        <form method="get" action="">
+            <p><label for ="Text1">Field 1: </label>
+                <input name="Text1" type="text" id="Text1" 
+                placeholder="Placeholder Text 1"/></p>
+            <p><label for ="Text1">Field 1: </label>
+                <input name="Text2" type="email" id="Text2" 
+                placeholder="Placeholder Text 2"/></p>
+            <p><label for ="Text1">Field 1: </label>
+                <input name="Text3" type="url" id="Text3" 
+                placeholder="Placeholder Text 3"/></p>
+        </form>
+    </div>
+
+    <footer>
+     <p>&copy; Copyright  by bricklerml</p>
+    </footer>
+  </div>
+</body>
+</html>
