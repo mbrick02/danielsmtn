293aabdd303c66fb3f4c9eb5afc5bc1345b2af79
michaelbrickler <mbrick02@gmail.com>
2013-09-10 17:21:35 -0400

    errno()

--- testconn.php
+++ testconn.php
@@ -5,7 +5,7 @@ define("DB_USER", "mbrick02");
 define("DB_PASS", "Job4Fau");
 define("DB_NAME", "usufructdish");
 $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-if(mysqli_connect_errn()){
+if(mysqli_connect_errno()){
 	die("DB connect failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")");
 } else {
 	echo "DMDB connected";
