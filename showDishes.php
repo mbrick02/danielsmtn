dca05790ab308b80f8224644a4e8c08263cc2ad1
michaelbrickler <mbrick02@gmail.com>
2013-09-11 17:35:01 -0400

    showDishes php list

--- showDishes.php
+++ showDishes.php
@@ -1,14 +1,30 @@
 <?php require_once("./includes/functions.php");	?>
+<?php require_once("./includes/connection.php");	?>
 <?php 
 	$query = "SELECT * ";
 	$query .= "FROM dish ";
-	echo $query;
+	// echo $query;
+	$result = mysqli_query($connection, $query);
 ?>
 <?php include("./includes/layout/header.php");	?>
 <div id="main">
 	<navigation id="navigation">
 		&nbsp;
 	</navigation>
-	<content id="page"></content>
+	<content id="page">
+		<ul>
+		<?php 
+			while ($dish = mysqli_fetch_assoc($result)){
+				
+		
+		?>
+			<li>
+				<?php echo $dish["lname"] . ": " . $dish["dish"] ?>
+			</li>
+		<?php } ?>
+		</ul>
+	</content>
 </div>
+<?php mysqli_free_result($result); ?>
+<?php mysqli_close($connection); ?>
 <?php include("./includes/layout/footer.php");	?>
