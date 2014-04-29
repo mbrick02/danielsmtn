ac3f9245673b64409ee7d142e70e5857f675ed98
michaelbrickler <mbrick02@gmail.com>
2014-04-29 18:01:17 -0400

    positioned invite text and started testing tabs

--- public/javascripts/U14code.js
+++ public/javascripts/U14code.js
@@ -4,11 +4,34 @@
  */
 
 $(document).ready(function() {
-	$("p").hide();
-	$("h2").click(function() {
-		$(this).nextAll().slideToggle(300);
-	});
+	$("#tabs").tabs();
+	
+	$("#inviteTab div p").hide();
+
+	$("#inviteTab div h2").nextAll().fadeIn(2000);
+
+/*
+	?Do I want to use image width pecentage for placement of text?
 	var imgUProgBkgrnd = document.getElementById('uProgBkgrnd');
 	var imgWidth = imgUProgBkgrnd.clientWidth;
 	console.log('image width: ' + imgWidth);
+*/
 });
+
+/*
+Tried this first:
+	$("h2").click(function() {
+		$(this).nextAll().slideToggle(300);
+	});
+	
+Chaining sample code:	
+$(document).ready(function () {
+	var $grid = $('#bab_grid');
+	var $lists = $grid.find('li');
+
+	$lists.css('background', '#600');
+	$lists.animate({width: '-=100'}, 2000);
+	$lists.fadeOut()
+	$lists.fadeIn('slow');
+});
+*/
